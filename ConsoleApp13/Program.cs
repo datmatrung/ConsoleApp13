b5aeb1bf335db8e0a351fef467d5c08a702abc45
datmatrung <datmatrung@gmail.com>
2020-04-22 17:15:27 +0700

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Node
+    {
+        int number;
+        Node next;
+        public Node(int value)
+        {
+            number = value;
+            next = null;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node node = new Node(15);
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
